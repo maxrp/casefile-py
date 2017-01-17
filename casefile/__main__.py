33860050287bab2243eb5e699557cb1a3b02d1c6
Max R.D. Parmer <maxp@trystero.is>
2017-01-17 14:47:52 -0800

    Implement UI for case listing.

--- casefile/__main__.py
+++ casefile/__main__.py
@@ -1,8 +1,9 @@
 import argparse
+import sys
 
 from . import __version__
 from .config import read_config
-from .casefile import new_case
+from .casefile import new_case, list_cases
 
 
 def main():
@@ -15,12 +16,24 @@ def main():
                         '--version',
                         action='version',
                         version=version.format(__version__))
-    parser.add_argument('summary',
-                        nargs="?",
-                        help="A brief case summary.")
+    parser.add_argument('-l',
+                        '--list-cases',
+                        action='store_true',
+                        )
+    subparsers = parser.add_subparsers(help="subcommands")
+    new_case_parser = subparsers.add_parser('new', help="New case.")
+    new_case_parser.add_argument('summary',
+                                 nargs="?",
+                                 help="A brief case summary.")
     args = parser.parse_args()
     config = read_config()
-    new_case(args.summary, config['casefile'])
+    if args.list_cases:
+        for case in list_cases(config['casefile']):
+            print("{}:\n\t{}".format(*case))
+        sys.exit(0)
+
+    if hasattr(args, 'summary'):
+        new_case(args.summary, config['casefile'])
 
 if __name__ == "__main__":
     main()
