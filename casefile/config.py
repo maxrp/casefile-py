278ca7d7a6ff60025b3f39045208cf290af5cc53
Max R.D. Parmer <maxp@trystero.is>
2019-11-01 12:52:55 -0700

    Simplifies config searching logic, fixes some quotes

--- casefile/config.py
+++ casefile/config.py
@@ -15,26 +15,31 @@ CasefileConfigParser = partial(ConfigParser,
                                interpolation=None)
 
 
-def _default_config_file(config_name='casefile.ini'):
+def find_config(config_name='casefile.ini'):
     xdg_cfg_home = getenv('XDG_CONFIG_HOME')
     dot_config = Path.home() / '.config'
+    local_config = Path.cwd() / config_name
 
     if xdg_cfg_home:
         cfg = Path(xdg_cfg_home) / config_name
     elif dot_config.exists():
         cfg = dot_config / config_name
     elif platform.startswith('linux') or ('bsd' in platform):
-        cfg = Path.home() / f".{config_name}"
+        cfg = Path.home() / f'.{config_name}'
     elif platform.startswith('darwin'):
         cfg = Path.home() / 'Library' / 'is.trystero.CaseFile' / config_name
     else:
-        raise Exception(f"{platform} support is not implemented.")
+        raise Exception(f'{platform} support is not implemented.')
+
+    # Or override it all with a local config
+    if local_config.exists():
+        cfg = local_config
 
     return cfg
 
 
 def _write_config():
-    default_config = _default_config_file()
+    default_config = find_config()
 
     if not default_config.parent.exists():
         default_config.parent.mkdir()
@@ -50,34 +55,26 @@ def _write_config():
         }
         config.write(new_config_file)
 
+    print(f'Created default config in {default_config}')
     return default_config
-
-
-def find_config(config_name='casefile.ini'):
-    """Try to find a :config_name: in the default locations of
-    ./:config_name: and failing that ~/$XDG_CONFIG_HOME/casefile.ini
-    """
-    default_configs = [Path.cwd() / config_name,
-                       _default_config_file(config_name)]
-    for config in default_configs:
-        if config.exists():
-            return config
-    raise FileNotFoundError(default_configs[0])
 
 
 def read_config():
     """Try to read the configuration file, and if this fails
     write out a default config.
     """
-    config = CasefileConfigParser()
-
     try:
         config_file = find_config()
-    except FileNotFoundError:
-        # try to create a config
-        config_file = _write_config()
-        print('Created default config in {}'.format(config_file))
+    except:
+        raise
+    finally:
+        try:
+          # try to create a config
+          config_file = _write_config()
+        except:
+          raise
 
+    config = CasefileConfigParser()
     with config_file.open('r') as cfg:
         config.read_file(cfg)
 
