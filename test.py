10536c10c4d47202f411953199c964d1f16d9873
Max R.D. Parmer <maxp@trystero.is>
2021-04-17 12:52:22 -0700

    Reorganize tests/imports a bit, hold platform constant

--- test.py
+++ test.py
@@ -1,64 +1,68 @@
 import os
 
 from pathlib import Path
-from unittest import mock
+from unittest.mock import patch
 
 import pytest
 
-from casefile.config import find_config
+from casefile.config import find_config, write_config
 from casefile.errors import UnsupportedPlatform
 
-DEFAULT_CONF = "casefile.ini"
 
+class TestConfig:
+    """Test casefile.config components"""
 
-def test_xdg_find_config(tmpdir):
-    """Test config discovery on linux under XDG"""
-    expected_config = tmpdir / DEFAULT_CONF
+    config_file = "casefile.ini"
 
-    with mock.patch.dict(os.environ, {"XDG_CONFIG_HOME": str(tmpdir)}):
-        config_path = find_config()
+    def test_xdg_find_config(self, tmpdir):
+        """Test config discovery on linux under XDG"""
+        expected_config = tmpdir / self.config_file
 
-    assert config_path == expected_config
+        with patch("casefile.config.platform", "linux"):
+            # assuming linux (or bsd...)
+            with patch.dict(os.environ, {"XDG_CONFIG_HOME": str(tmpdir)}):
+                config_path = find_config()
 
+        assert config_path == expected_config
 
-def test_home_find_dotconfig(tmpdir):
-    """Test discovery of ~/.config/casefile.ini"""
-    expected_config = tmpdir.mkdir(".config") / DEFAULT_CONF
-    expected_config.write("test")
+    def test_home_find_dotconfig(self, tmpdir):
+        """Test discovery of ~/.config/casefile.ini"""
+        expected_config = tmpdir.mkdir(".config") / self.config_file
+        expected_config.write("test")
 
-    with mock.patch.dict(os.environ, {"HOME": str(tmpdir)}):
-        config_path = find_config()
+        with patch("casefile.config.platform", "linux"):
+            with patch.dict(os.environ, {"HOME": str(tmpdir)}):
+                config_path = find_config()
 
-    assert config_path == expected_config
+        assert config_path == expected_config
 
+    def test_home_find_dotfile(self, tmpdir):
+        """Test discovery of ~/.casefile.ini"""
+        expected_config = tmpdir / f".{self.config_file}"
 
-def test_home_find_dotfile(tmpdir):
-    """Test discovery of ~/.casefile.ini"""
-    expected_config = tmpdir / f".{DEFAULT_CONF}"
+        with patch("casefile.config.platform", "linux"):
+            with patch.dict(os.environ, {"HOME": str(tmpdir)}):
+                config_path = find_config()
 
-    with mock.patch.dict(os.environ, {"HOME": str(tmpdir)}):
-        config_path = find_config()
+        assert config_path == expected_config
 
-    assert config_path == expected_config
+    def test_find_macos_native_config(self, tmpdir):
+        """Test discovery of $PWD/casefile.ini"""
+        expected_config = (
+            Path.home()
+            / "Library"
+            / "Application Support"
+            / "is.trystero.CaseFile"
+            / self.config_file
+        )
 
+        with patch("casefile.config.platform", "darwin"):
+            config_path = find_config()
 
-def test_find_macos_native_config(tmpdir):
-    """Test discovery of $PWD/casefile.ini"""
-    expected_config = (
-        Path.home()
-        / "Library"
-        / "Application Support"
-        / "is.trystero.CaseFile"
-        / DEFAULT_CONF
-    )
+        assert config_path == expected_config
 
-    with mock.patch("casefile.config.platform", "darwin"):
-        config_path = find_config()
-
-    assert config_path == expected_config
-
-
-def test_unsupported_platform_config():
-    with pytest.raises(UnsupportedPlatform):
-        with mock.patch("casefile.config.platform", "win32"):
-            find_config()
+    def test_unsupported_platform_config(self):
+        """Test rejection of unsupported platforms."""
+        with pytest.raises(UnsupportedPlatform):
+            with patch("casefile.config.platform", "win32"):
+                find_config()
